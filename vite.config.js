c0390d756acd77690ca6d937916440d5e05ce422
Anand <anand2522004@gmail.com>
2025-05-15 01:29:57 +0530

    Fix: added base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ai-ChatBot/', // Corrected base path
+  base: './', // Corrected base path
 });
